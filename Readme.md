500d075cc7e09e66b86016b7cb06870918518d95
Luis Gonzalez <luisarturogonzalezvalencia@gmail.com>
2024-04-14 17:07:22 -0300

    Taks repo ready

--- Readme.md
+++ Readme.md
@@ -9,3 +9,9 @@ Comitear cambios:
 - git add -A
 - git commit -m "name commit"
 - git push
+
+
+Actividad
+- Agregar tests para probar las funciones de subtract, multiply y divide.
+- Corregir la funcion de divide para las divisiones por 0
+- Comitear y observar si pasan los test en github actions
--- calculator.test.ts
+++ calculator.test.ts
@@ -1,20 +1,8 @@
 // calculator.test.ts
-import { add, subtract, multiply, divide } from './calculator';
+import { add, divide } from './calculator';
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
-});
-
-test('subtracts 2 - 1 to equal 1', () => {
-    expect(subtract(2, 1)).toBe(1);
-});
-
-test('multiplies 2 * 3 to equal 6', () => {
-    expect(multiply(2, 3)).toBe(6);
-});
-
-test('divides 4 / 2 to equal 2', () => {
-    expect(divide(4, 2)).toBe(2);
 });
 
 test('divides 4 / 0 to throw error', () => {
